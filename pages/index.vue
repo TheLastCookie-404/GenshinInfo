650ce866d01427e31c506fa431534f696cb3c2af
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-03-24 23:08:24 +0500

    Tech

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="bg-base-100 flex h-[80vh] gap-3">
-    <div
-      v-for="(n, index) in 5"
-      :key="index"
-      class="rounded-box bg-neutral xs:bg-base-300 h-fit w-fit p-3">
-      Lol
-    </div>
+  <div v-auto-animate class="bg-base-100 flex h-[80vh] gap-3 p-3">
+    <button
+      v-for="elem in tiles"
+      :key="elem"
+      class="btn btn-neutral"
+      @click="tiles = removeItem(elem, tiles)">
+      {{ elem }}
+    </button>
+    <button class="btn btn-success" @click="tiles = tilesDefault">
+      Resset
+    </button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const tilesDefault: string[] = ["lol", "kek", "azaza", "amogu", "buntus"];
+  const tiles = ref<string[]>(tilesDefault);
+
+  useAutoAnimate({ duration: 200 });
+
+  function removeItem(toRemove: string, arr: string[]) {
+    return arr.filter((item) => item !== toRemove);
+  }
+</script>
